3d4aeb82d03262e2d1c297165a2bb194c075c3a7
Anders <42707128+AndersRo@users.noreply.github.com>
2018-09-21 16:19:41 -0500

    update
    update

--- WebArife/WebGPS/app/Cliente.php
+++ WebArife/WebGPS/app/Cliente.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    protected $table = 'cliente';
+    protected $table = 'client';
 
     protected $primarykey = 'IdCliente';
 
