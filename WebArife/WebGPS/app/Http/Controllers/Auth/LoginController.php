5bce4d33c55cc290d827fee02399d989d846e3f3
AndersRo <anroqui2011@gmail.com>
2018-11-03 03:32:40 -0500

    li

--- WebArife/WebGPS/app/Http/Controllers/Auth/LoginController.php
+++ WebArife/WebGPS/app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,7 @@
 namespace webGps\Http\Controllers\Auth;
 
 use Auth;
-//use DB;
+use DB;
 use webGps\User;
 use Illuminate\Http\Request;
 use webGps\Http\Controllers\Controller;
@@ -18,14 +18,30 @@ class LoginController extends Controller
   public function showLoginForm(Request $request){
     return view('auth/login');
   }
-  public function login(){
-      $credentials = $this->validate(request(),[
-        'email'=>'email|required|string',
+  public function login(Request $request){
+    /*$credentials = array(
+      $usr= $request->input('email'),
+      $pass= $request->input('password')
+    );
+    return $credentials;*/
+
+      $query=trim($request->get('email'));
+      $users=DB::table('users')
+      //->select('email','password')
+      ->where('email','LIKE','%'.$query.'%')
+      ->orderBy('id','desc')
+      ->paginate(7);
+        return $users;
+
+
+
+      /*$credentials = $this->validate(request(),[
+        'email'=>'required|string',
         'password'=>'required|string'
-      ]);
-      if (Auth::attempt($credentials)) {
-        return 'Tu sesion ha iniciado';
-      }
+      ]);*/
+      //if (Auth::attempt($credentials)) {
+        //return $credentials;
+      //}
         return back()
         ->withErrors(['email'=> trans('auth.failed')])
         ->withInput(request(['email']));
